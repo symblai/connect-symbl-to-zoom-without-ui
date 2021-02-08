bceca75fe58bf49cceb14aceb0b606e4a0d407ca
Adam Voliva <adam.voliva@symbl.ai>
2021-02-08 15:09:37 -0800

    Update code.

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
 const {sdk, SpeakerEvent} = require("symbl-node");
+const appId = appId;
+const appSecret = appSecret;
 
 sdk.init({
-  appId: 'appId',
-  appSecret: 'appSecret',
+  appId: appId,
+  appSecret: appSecret,
   basePath: "https://api.symbl.ai",
-}).then((e) => {
-  console.log('SDK initialized.', e);
+}).then(async() => {
+  console.log('SDK initialized.');
   try {
     const DTMF_MEETING_ID = "ZOOM_MEETING_ID";
-	const DTMF_MEETING_PASSCODE = "ZOOM_MEETING_PASSCODE";
-	const dtmfSequence = `,,${DTMF_MEETING_ID}#,,${DTMF_MEETING_PASSCODE}`;
-	sdk.startEndpoint({
-	  operation: "start",
-	  endpoint: {
-	    type: "pstn",
-	    phoneNumber: "+16465588656", // US Zoom Numbers are "+16465588656", or "+14086380968".
-	    dtmf: dtmfSequence,
-	  },
-	  actions: [
-	    {
-	      invokeOn: "stop",
-	      name: "sendSummaryEmail",
-	      parameters: {
-	        emails: [EMAIL_ADDRESS],
-	      },
-	    },
-	  ],
-	  data: {
-	    session: {
-	      name: "Symbl Zoom Test",
-	    },
-	  },
-	}).then((connection) => {
-	  const connectionId = connection.connectionId;
-	  console.log("Successfully connected.", connectionId);
-	  console.log("Calling into Zoom now, please wait.");
-	})
-	.catch((err) => {
-	   console.error("Error while starting the connection", err);
-	});
+    const DTMF_MEETING_PASSCODE = "ZOOM_MEETING_PASSCODE";
+    const dtmfSequence = `,,${DTMF_MEETING_ID}#,,${DTMF_MEETING_PASSCODE}#`;
+    const phoneNumber = "ZOOM_PHONE_NUMBER"; // US Zoom Numbers are "+16465588656", or "+14086380968".
+
+    sdk.startEndpoint({
+      endpoint: {
+        type: "pstn",
+        phoneNumber: phoneNumber,
+        dtmf: dtmfSequence,
+      },
+      actions: [
+        {
+          invokeOn: "stop",
+          name: "sendSummaryEmail",
+          parameters: {
+            emails: [EMAIL_ADDRESS],
+          },
+        },
+      ],
+      data: {
+        session: {
+          name: meetingName,
+        },
+      },
+    }).then((connection) => {
+      const connectionId = connection.connectionId;
+      console.log("Successfully connected.", connectionId);
+      console.log("Calling into Zoom now, please wait.");
+    })
+    .catch((err) => {
+       console.error("Error while starting the connection", err);
+    });
   } catch (e) {
     console.error(e);
   }
